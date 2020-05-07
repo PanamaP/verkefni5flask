ab2ef8b39ff008bed624a67f098dab5af7334613
PanamaP <elfarsnaer@gmail.com>
2020-05-07 21:09:54 +0000

    laga database

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import pyrebase
-from config import api_key
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'covid_19'
 
 config = {
-    "apiKey": api_key,
-    "authDomain": "verkefni5-49d08.firebaseapp.com",
-    "databaseURL": "https://verkefni5-49d08.firebaseio.com",
-    "projectId": "verkefni5-49d08",
-    "storageBucket": "verkefni5-49d08.appspot.com",
-    "messagingSenderId": "917292550423",
-    "appId": "1:917292550423:web:cea8311171c957200acf52",
-    "measurementId": "G-V0KWD32QZ6"
+    "apiKey": "AIzaSyDKnn19CNEjmz8tqxCNGTUWnrT99J78tn4",
+    "authDomain": "verkefni5fix.firebaseapp.com",
+    "databaseURL": "https://verkefni5fix.firebaseio.com",
+    "projectId": "verkefni5fix",
+    "storageBucket": "verkefni5fix.appspot.com",
+    "messagingSenderId": "610557186352",
+    "appId": "1:610557186352:web:70e5bd86c53091770b38f5",
+    "measurementId": "G-8C2FRP5L2Q"
 }
 
 fb = pyrebase.initialize_app(config)
@@ -92,7 +91,7 @@ def topsecret():
 if __name__ == "__main__":
     app.run(debug=True)
 
-#skrifum nýjan í grunn hnútur sem heitir notandi
+# skrifum nýjan í grunn hnútur sem heitir notandi
 # db.child("notandi").push({"notendanafn":"dsg", "lykilorð":1234})
 
 # # förum í grunn og sækjum allar raðir ( öll gögn )
